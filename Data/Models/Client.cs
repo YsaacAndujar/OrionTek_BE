7fd1e40c50e2f00273fda3783d92489b2947a485
Ysaac Andujar Binet <ysaac@codika.net>
2024-10-24 23:44:58 -0400

    client, directions required fields

--- Data/Models/Client.cs
+++ Data/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Data.Models
 {
     public class Client : GenericEntity
     {
+        [Required]
         public string Name { get; set; }
         public List<Direction> Directions { get; set; }
     }
--- Data/Models/Direction.cs
+++ Data/Models/Direction.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Data.Models
 {
     public class Direction
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int ClientId { get; set; }
         public Client Client { get; set; }
