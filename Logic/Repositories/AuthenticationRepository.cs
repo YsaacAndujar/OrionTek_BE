e2d3eab3a612009d1ceb15171af1377103f2d9c7
Ysaac Andujar Binet <ysaac@codika.net>
2024-10-25 14:16:08 -0400

    finish

--- Logic/Repositories/AuthenticationRepository.cs
+++ Logic/Repositories/AuthenticationRepository.cs
@@ -153,7 +153,7 @@ namespace Logic.Repositories
             });
             await dbContext.SaveChangesAsync();
             var frontUrl = configuration["FrontEndUrl"];
-            var redirectUrl = $"{frontUrl}recover-password?userid={user.Id}&code={code}";
+            var redirectUrl = $"{frontUrl}auth/recover-password?userid={user.Id}&code={code}";
             await emailSender.SendEmail(
                 toEmail: user.Email,
                 toName: user.Email,
