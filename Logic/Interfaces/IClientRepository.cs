7a52298276c41d42d881c181a3e0ff58e82a4aa4
Ysaac Andujar Binet <ysaac@codika.net>
2024-10-24 21:47:50 -0400

    repositories

---
+++ Logic/Interfaces/IClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using Logic.Dtos.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<ClientDto> Create(ClientCreateDto dto);
+        Task<List<ClientDto>> GetAll(ClientFilterDto dto);
+        Task Update(int id, ClientCreateDto dto);
+        Task<ClientDto> Get(int id);
+        Task Delete(int id);
+    }
+}
---
+++ Logic/Repositories/ClientRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Data;
+using Logic.Dtos.Clients;
+using Logic.Interfaces;
+
+namespace Logic.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly IMapper mapper;
+        private readonly OrionTekDbContext dbContext;
+
+        public ClientRepository(IMapper _mapper, OrionTekDbContext _dbContext)
+        {
+            mapper = _mapper;
+            dbContext = _dbContext;
+        }
+        public Task<ClientDto> Create(ClientCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClientDto> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ClientDto>> GetAll(ClientFilterDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, ClientCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
